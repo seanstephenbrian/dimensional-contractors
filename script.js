98033bee9d07320b2b5cb23474aad12e5ed30df0
seanstephenbrian <seanstephenbrian@gmail.com>
2022-10-19 15:00:52 -0400

    Create initial version of drywall square graphic

---
+++ script.js
@@ -0,0 +1,30 @@
+const squareX = document.querySelector('.square-x');
+const squareY = document.querySelector('.square-y');
+
+function createXLines() {
+    for (let i=0; i<80; i++) {
+        const newDiv = document.createElement('div');
+        if (i % 2) {
+            newDiv.classList.add('square-x-odd');
+        } else {
+            newDiv.classList.add('square-x-even');
+        }
+        squareX.appendChild(newDiv);
+    }
+}
+
+createXLines();
+
+function createYLines() {
+    for (let i=0; i<30; i++) {
+        const newDiv = document.createElement('div');
+        if (i % 2) {
+            newDiv.classList.add('square-y-odd');
+        } else {
+            newDiv.classList.add('square-y-even');
+        }
+        squareY.appendChild(newDiv);
+    }
+}
+
+createYLines();
