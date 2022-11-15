8c34729df899697e8285fee57fab1e5ff14b7e30
sean stephen brian <seanstephenbrian@gmail.com>
2022-11-15 12:02:19 -0500

    set up 'module' for page render functions

--- script.js
+++ script.js
@@ -1,36 +1,83 @@
-const squareX = document.querySelector('.square-x');
-const squareY = document.querySelector('.square-y');
+const Page = (function() {
 
-function createXLines() {
-    for (let i=0; i<80; i++) {
-        const newDiv = document.createElement('div');
-        if (i % 2) {
-            newDiv.classList.add('square-x-odd');
-        } else {
-            newDiv.classList.add('square-x-even');
+    const squareX = document.querySelector('.square-x');
+    const squareY = document.querySelector('.square-y');
+    const content = document.querySelector('.content');
+
+    (function createXLines() {
+        for (let i=0; i<80; i++) {
+            const newDiv = document.createElement('div');
+            if (i % 2) {
+                newDiv.classList.add('square-x-odd');
+            } else {
+                newDiv.classList.add('square-x-even');
+            }
+            squareX.appendChild(newDiv);
         }
-        squareX.appendChild(newDiv);
-    }
-}
-
-function createYLines() {
-    for (let i=0; i<30; i++) {
-        const newDiv = document.createElement('div');
-        if (i % 2) {
-            newDiv.classList.add('square-y-odd');
-        } else {
-            newDiv.classList.add('square-y-even');
+    })();
+    
+    (function createYLines() {
+        for (let i=0; i<30; i++) {
+            const newDiv = document.createElement('div');
+            if (i % 2) {
+                newDiv.classList.add('square-y-odd');
+            } else {
+                newDiv.classList.add('square-y-even');
+            }
+            squareY.appendChild(newDiv);
         }
-        squareY.appendChild(newDiv);
+    })();
+    
+    (function fillFooterText() {
+        const year = new Date().getFullYear();
+        const footer = document.querySelector('footer');
+        footer.textContent = `Copyright © Dimensional Contractors Inc. ${year}`;
+    })();
+
+    function clearContent() {
+        content.innerHTML = '';
     }
-}
 
-function fillFooterText() {
-    const year = new Date().getFullYear();
-    const footer = document.querySelector('footer');
-    footer.textContent = `Copyright © Dimensional Contractors Inc. ${year}`;
-}
+    function renderHome() {
+        clearContent();
 
-createXLines();
-createYLines();
-fillFooterText();
+        content.innerHTML = 'worked';
+    }
+
+    function renderCommercial() {
+        clearContent();
+    
+    }
+    
+    function renderResidential() {
+        clearContent();
+    
+    }
+    
+    function renderPortfolio() {
+        clearContent();
+    
+    }
+    
+    function renderStory() {
+        clearContent();
+    
+    }
+    
+    function renderContact() {
+        clearContent();
+    
+    }
+
+    return {
+        renderHome,
+        renderCommercial,
+        renderResidential,
+        renderPortfolio,
+        renderStory,
+        renderContact
+    }
+
+})();
+
+Page.renderHome();
