14dc291cda794f2f883fe45badda44129a42420e
sean stephen brian <seanstephenbrian@gmail.com>
2022-11-16 10:17:51 -0500

    rewrite photo-related javascript
    eliminate repeated code, create new functions to immediately get
    specific categories of photos ... basically just make the code more
    reasonable

--- script.js
+++ script.js
@@ -62,9 +62,13 @@ const Page = (function() {
 
     function addSliderListeners() {
         const back = document.querySelector('.back');
-        back.addEventListener('click', Photos.goBack);
+        back.addEventListener('click', () => {
+            Photos.changePhoto('back');
+        });
         const forward = document.querySelector('.forward');
-        forward.addEventListener('click', Photos.goForward);
+        forward.addEventListener('click', () => {
+            Photos.changePhoto('forward');
+        });
     }
 
     function clearContent() {
@@ -99,79 +103,55 @@ const Page = (function() {
             const contactUs = document.querySelector('.inline-contact');
             contactUs.addEventListener('click', renderContact);
         }
+    
+    function loadFirstImage(category) {
+        let selectedPhotos;
+        if (category === 'commercial') {
+            selectedPhotos = Photos.getCommercialPhotos();
+        } else if (category === 'residential') {
+            selectedPhotos = Photos.getResidentialPhotos();
+        } else if (category === 'portfolio') {
+            selectedPhotos = Photos.getPortfolioPhotos();
+        }
+
+        const caption = document.querySelector('.photo-caption');
+        caption.textContent = selectedPhotos[0].caption;
+
+        const photo = document.querySelector('.photo');
+        photo.src = selectedPhotos[0].src;
+        photo.dataset.id = 0;
+        photo.dataset.category = category;
+    }
 
     function renderCommercial() {
         clearContent();
         content.classList.add('commercial-content');
         fillContent('html/commercial.html')
-            .then(loadFirstCommercialImage)
+            .then(() => {
+                loadFirstImage('commercial');
+            })
             .then(addSliderListeners);
     }
 
-        function loadFirstCommercialImage() {
-            const photos = Photos.getPhotoList();
-            const commercialPhotos = photos.filter(photo => {
-                if (photo.category === 'commercial') {
-                    return true;
-                } 
-                return false;
-            });
-            const caption = document.querySelector('.photo-caption');
-            caption.textContent = commercialPhotos[0].caption;
-            const photo = document.querySelector('.photo');
-            photo.src = commercialPhotos[0].src;
-            photo.dataset.id = 0;
-            photo.dataset.category = 'commercial';
-        }
-    
     function renderResidential() {
         clearContent();
         content.classList.add('residential-content');
         fillContent('html/residential.html')
-            .then(loadFirstResidentialImage)
+            .then(() => {
+                loadFirstImage('residential');
+            })
             .then(addSliderListeners);
     }
-
-        function loadFirstResidentialImage() {
-            const photos = Photos.getPhotoList();
-            const residentialPhotos = photos.filter(photo => {
-                if (photo.category === 'residential') {
-                    return true;
-                } 
-                return false;
-            });
-            const caption = document.querySelector('.photo-caption');
-            caption.textContent = residentialPhotos[0].caption;
-            const photo = document.querySelector('.photo');
-            photo.src = residentialPhotos[0].src;
-            photo.dataset.id = 0;
-            photo.dataset.category = 'residential';
-            photo.dataset.portfolio = residentialPhotos[0].portfolio;
-        }
     
     function renderPortfolio() {
         clearContent();
         content.classList.add('portfolio-content');
         fillContent('html/portfolio.html')
-            .then(loadFirstPortfolioImage)
+            .then(() => {
+                loadFirstImage('portfolio');
+            })
             .then(addSliderListeners);
     }
-
-        function loadFirstPortfolioImage() {
-            const photos = Photos.getPhotoList();
-            const portfolioPhotos = photos.filter(photo => {
-                if (photo.portfolio === true) {
-                    return true;
-                } 
-                return false;
-            });
-            const caption = document.querySelector('.photo-caption');
-            caption.textContent = portfolioPhotos[0].caption;
-            const photo = document.querySelector('.photo');
-            photo.src = portfolioPhotos[0].src;
-            photo.dataset.id = 0;
-            photo.dataset.portfolio = 'true';
-        }
     
     function renderStory() {
         clearContent();
@@ -207,113 +187,78 @@ const Photos = (function() {
         photos.push(newPhoto);
     }
 
-    function getPhotoList() {
+    function getAllPhotos() {
         return photos;
     }
 
+    function getCommercialPhotos() {
+        return photos.filter(photo => {
+            return photo.category === 'commercial' ? true : false;
+        });
+    }
 
-            // should write functions to get all photos, residential photos, commercial photos, portfolio photos
+    function getResidentialPhotos() {
+        return photos.filter(photo => {
+            return photo.category === 'residential' ? true : false;
+        })
+    }
 
+    function getPortfolioPhotos() {
+        return photos.filter(photo => {
+            return photo.portfolio === true ? true : false;
+        })
+    }
 
-    // function changePhoto(direction) {
-    //     const currentPhoto = document.querySelector('.photo');
-    //     const currentCategory = currentPhoto.dataset.category;
-    //     const portfolioStatus = currentPhoto.dataset.portfolio;
-    //     const allCategoryPhotos = photos.filter(photo => {
-    //         if (portfolioStatus === 'true') {
-    //             return true;
-    //         } else if ((!portfolioStatus || portfolioStatus === 'false') && photo.category === currentCategory) {
-    //             return true;
-    //         }
-    //         return false;
-    //     });
-    //     const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
-    //     let nextPhotoIndex;
-    //     if (direction === 'back') {
-    //         if (currentPhotoIndex === 0) {
-    //             nextPhotoIndex = allCategoryPhotos.length - 1;
-    //         } else {
-    //             nextPhotoIndex = currentPhotoIndex - 1;
-    //         }
-    //     } else if (direction === 'forward') {
-    //         if (currentPhotoIndex + 1 === allCategoryPhotos.length) {
-    //             nextPhotoIndex = 0;
-    //         } else {
-    //             nextPhotoIndex = currentPhotoIndex + 1;
-    //         }
-    //     }
-    //     const nextPhoto = allCategoryPhotos[nextPhotoIndex];
-    //     currentPhoto.src = nextPhoto.src;
-    //     currentPhoto.dataset.id = nextPhotoIndex;
-    //     const caption = document.querySelector('.photo-caption');
-    //     caption.textContent = nextPhoto.caption;
-    // }
+    function changePhoto(direction) {
+        const currentPhoto = document.querySelector('.photo');
+        const currentCategory = currentPhoto.dataset.category;
 
-    // function goBack() {
-    //     changePhoto('back');
-    //     // const currentPhoto = document.querySelector('.photo');
-    //     // const currentCategory = currentPhoto.dataset.category;
-    //     // const portfolioStatus = currentPhoto.dataset.portfolio;
-    //     // const allCategoryPhotos = photos.filter(photo => {
-    //     //     if (portfolioStatus) {
-    //     //         return true;
-    //     //     } else if (!portfolioStatus && photo.category === currentCategory) {
-    //     //         return true;
-    //     //     }
-    //     //     return false;
-    //     // });
-    //     // const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
-    //     // let nextPhotoIndex;
-    //     // if (currentPhotoIndex === 0) {
-    //     //     nextPhotoIndex = allCategoryPhotos.length - 1;
-    //     // } else {
-    //     //     nextPhotoIndex = currentPhotoIndex - 1;
-    //     // }
-    //     // const nextPhoto = allCategoryPhotos[nextPhotoIndex];
-    //     // currentPhoto.src = nextPhoto.src;
-    //     // currentPhoto.dataset.id = nextPhotoIndex;
-    //     // const caption = document.querySelector('.photo-caption');
-    //     // caption.textContent = nextPhoto.caption;
-    // }
+        let selectedPhotos;
+        if (currentCategory === 'commercial') {
+            selectedPhotos = getCommercialPhotos();
+        } else if (currentCategory === 'residential') {
+            selectedPhotos = getResidentialPhotos();
+        } else if (currentCategory === 'portfolio') {
+            selectedPhotos = getPortfolioPhotos();
+        }
 
-    // function goForward() {
-    //     changePhoto('forward');
-    //     // const currentPhoto = document.querySelector('.photo');
-    //     // const currentCategory = currentPhoto.dataset.category;
-    //     // const portfolioStatus = currentPhoto.dataset.portfolio;
-    //     // const allCategoryPhotos = photos.filter(photo => {
-    //     //     if (portfolioStatus === 'true') {
-    //     //         return true;
-    //     //     } else if (portfolioStatus === 'false' && photo.category === currentCategory) {
-    //     //         return true;
-    //     //     }
-    //     //     return false;
-    //     // });
-    //     // const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
-    //     // let nextPhotoIndex;
-    //     // if (currentPhotoIndex + 1 === allCategoryPhotos.length) {
-    //     //     nextPhotoIndex = 0;
-    //     // } else {
-    //     //     nextPhotoIndex = currentPhotoIndex + 1;
-    //     // }
-    //     // const nextPhoto = allCategoryPhotos[nextPhotoIndex];
-    //     // currentPhoto.src = nextPhoto.src;
-    //     // currentPhoto.dataset.id = nextPhotoIndex;
-    //     // const caption = document.querySelector('.photo-caption');
-    //     // caption.textContent = nextPhoto.caption;
-    // }
+        const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
+        let nextPhotoIndex;
+
+        if (direction === 'back') {
+            if (currentPhotoIndex === 0) {
+                nextPhotoIndex = selectedPhotos.length - 1;
+            } else {
+                nextPhotoIndex = currentPhotoIndex - 1;
+            }
+        } else if (direction === 'forward') {
+            if (currentPhotoIndex === selectedPhotos.length - 1) {
+                nextPhotoIndex = 0;
+            } else {
+                nextPhotoIndex = currentPhotoIndex + 1;
+            }
+        }
+        
+        const nextPhoto = selectedPhotos[nextPhotoIndex];
+        currentPhoto.src = nextPhoto.src;
+        currentPhoto.dataset.id = nextPhotoIndex;
+        const caption = document.querySelector('.photo-caption');
+        caption.textContent = nextPhoto.caption;
+    }
 
     // all photos:
     addPhoto('pags-ext.jpg', `Pag's Wine Bar (Doylestown, PA)`, 'commercial', false);
     addPhoto('pags-int.jpg', `Pag's Pub (Doylestown, PA)`, 'commercial', true);
     addPhoto('pags-int-2.jpg', `Pag's Pub (Doylestown, PA)`, 'residential', true);
-    addPhoto('santucci.jpg', `Santucci's Square Pizza (Warminster, PA)`, 'commercial', false);
+    addPhoto('santucci.jpg', `Santucci's Square Pizza (Warminster, PA)`, 'residential', false);
     addPhoto('serenity.jpg', `Serenity Day Spa (Doylestown, PA)`, 'commercial', true);
 
     return { 
-        getPhotoList,
-        goBack,
-        goForward
+        getAllPhotos,
+        getCommercialPhotos,
+        getResidentialPhotos,
+        getPortfolioPhotos,
+        changePhoto
     }
 
 })();
