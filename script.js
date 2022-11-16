00f6d690e7b7a9691cf841259302fdcc48716e10
sean stephen brian <seanstephenbrian@gmail.com>
2022-11-15 21:01:37 -0500

    try and fail to implement photo slider on all pgs
    need to simplify things and write some new functions to just return the
    photos needed, then process only those photos

--- script.js
+++ script.js
@@ -127,14 +127,51 @@ const Page = (function() {
     function renderResidential() {
         clearContent();
         content.classList.add('residential-content');
-        fillContent('html/residential.html');
+        fillContent('html/residential.html')
+            .then(loadFirstResidentialImage)
+            .then(addSliderListeners);
     }
+
+        function loadFirstResidentialImage() {
+            const photos = Photos.getPhotoList();
+            const residentialPhotos = photos.filter(photo => {
+                if (photo.category === 'residential') {
+                    return true;
+                } 
+                return false;
+            });
+            const caption = document.querySelector('.photo-caption');
+            caption.textContent = residentialPhotos[0].caption;
+            const photo = document.querySelector('.photo');
+            photo.src = residentialPhotos[0].src;
+            photo.dataset.id = 0;
+            photo.dataset.category = 'residential';
+            photo.dataset.portfolio = residentialPhotos[0].portfolio;
+        }
     
     function renderPortfolio() {
         clearContent();
         content.classList.add('portfolio-content');
-        fillContent('html/portfolio.html');
+        fillContent('html/portfolio.html')
+            .then(loadFirstPortfolioImage)
+            .then(addSliderListeners);
     }
+
+        function loadFirstPortfolioImage() {
+            const photos = Photos.getPhotoList();
+            const portfolioPhotos = photos.filter(photo => {
+                if (photo.portfolio === true) {
+                    return true;
+                } 
+                return false;
+            });
+            const caption = document.querySelector('.photo-caption');
+            caption.textContent = portfolioPhotos[0].caption;
+            const photo = document.querySelector('.photo');
+            photo.src = portfolioPhotos[0].src;
+            photo.dataset.id = 0;
+            photo.dataset.portfolio = 'true';
+        }
     
     function renderStory() {
         clearContent();
@@ -157,15 +194,16 @@ const Photos = (function() {
     let photos = [];
 
     // object constructor for new photos;
-    function Photo(src, caption, category) {
+    function Photo(src, caption, category, portfolio) {
         this.src = src;
         this.caption = caption;
-        this.category = category
+        this.category = category;
+        this.portfolio = portfolio;
     }
 
     // method to create new photo and add it to 'photos' array:
-    function addPhoto(src, caption, category) {
-        const newPhoto = new Photo(`img/photos/${src}`, caption, category);
+    function addPhoto(src, caption, category, portfolio) {
+        const newPhoto = new Photo(`img/photos/${src}`, caption, category, portfolio);
         photos.push(newPhoto);
     }
 
@@ -173,58 +211,104 @@ const Photos = (function() {
         return photos;
     }
 
-    function goBack() {
-        const currentPhoto = document.querySelector('.photo');
-        const currentCategory = currentPhoto.dataset.category;
-        const allCategoryPhotos = photos.filter(photo => {
-            if (photo.category === currentCategory) {
-                return true;
-            }
-            return false;
-        });
-        const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
-        let nextPhotoIndex;
-        if (currentPhotoIndex === 0) {
-            nextPhotoIndex = allCategoryPhotos.length - 1;
-        } else {
-            nextPhotoIndex = currentPhotoIndex - 1;
-        }
-        const nextPhoto = allCategoryPhotos[nextPhotoIndex];
-        currentPhoto.src = nextPhoto.src;
-        currentPhoto.dataset.id = nextPhotoIndex;
-        const caption = document.querySelector('.photo-caption');
-        caption.textContent = nextPhoto.caption;
-    }
 
-    function goForward() {
-        const currentPhoto = document.querySelector('.photo');
-        const currentCategory = currentPhoto.dataset.category;
-        const allCategoryPhotos = photos.filter(photo => {
-            if (photo.category === currentCategory) {
-                return true;
-            }
-            return false;
-        });
-        const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
-        let nextPhotoIndex;
-        if (currentPhotoIndex + 1 === allCategoryPhotos.length) {
-            nextPhotoIndex = 0;
-        } else {
-            nextPhotoIndex = currentPhotoIndex + 1;
-        }
-        const nextPhoto = allCategoryPhotos[nextPhotoIndex];
-        currentPhoto.src = nextPhoto.src;
-        currentPhoto.dataset.id = nextPhotoIndex;
-        const caption = document.querySelector('.photo-caption');
-        caption.textContent = nextPhoto.caption;
-    }
+            // should write functions to get all photos, residential photos, commercial photos, portfolio photos
+
+
+    // function changePhoto(direction) {
+    //     const currentPhoto = document.querySelector('.photo');
+    //     const currentCategory = currentPhoto.dataset.category;
+    //     const portfolioStatus = currentPhoto.dataset.portfolio;
+    //     const allCategoryPhotos = photos.filter(photo => {
+    //         if (portfolioStatus === 'true') {
+    //             return true;
+    //         } else if ((!portfolioStatus || portfolioStatus === 'false') && photo.category === currentCategory) {
+    //             return true;
+    //         }
+    //         return false;
+    //     });
+    //     const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
+    //     let nextPhotoIndex;
+    //     if (direction === 'back') {
+    //         if (currentPhotoIndex === 0) {
+    //             nextPhotoIndex = allCategoryPhotos.length - 1;
+    //         } else {
+    //             nextPhotoIndex = currentPhotoIndex - 1;
+    //         }
+    //     } else if (direction === 'forward') {
+    //         if (currentPhotoIndex + 1 === allCategoryPhotos.length) {
+    //             nextPhotoIndex = 0;
+    //         } else {
+    //             nextPhotoIndex = currentPhotoIndex + 1;
+    //         }
+    //     }
+    //     const nextPhoto = allCategoryPhotos[nextPhotoIndex];
+    //     currentPhoto.src = nextPhoto.src;
+    //     currentPhoto.dataset.id = nextPhotoIndex;
+    //     const caption = document.querySelector('.photo-caption');
+    //     caption.textContent = nextPhoto.caption;
+    // }
+
+    // function goBack() {
+    //     changePhoto('back');
+    //     // const currentPhoto = document.querySelector('.photo');
+    //     // const currentCategory = currentPhoto.dataset.category;
+    //     // const portfolioStatus = currentPhoto.dataset.portfolio;
+    //     // const allCategoryPhotos = photos.filter(photo => {
+    //     //     if (portfolioStatus) {
+    //     //         return true;
+    //     //     } else if (!portfolioStatus && photo.category === currentCategory) {
+    //     //         return true;
+    //     //     }
+    //     //     return false;
+    //     // });
+    //     // const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
+    //     // let nextPhotoIndex;
+    //     // if (currentPhotoIndex === 0) {
+    //     //     nextPhotoIndex = allCategoryPhotos.length - 1;
+    //     // } else {
+    //     //     nextPhotoIndex = currentPhotoIndex - 1;
+    //     // }
+    //     // const nextPhoto = allCategoryPhotos[nextPhotoIndex];
+    //     // currentPhoto.src = nextPhoto.src;
+    //     // currentPhoto.dataset.id = nextPhotoIndex;
+    //     // const caption = document.querySelector('.photo-caption');
+    //     // caption.textContent = nextPhoto.caption;
+    // }
+
+    // function goForward() {
+    //     changePhoto('forward');
+    //     // const currentPhoto = document.querySelector('.photo');
+    //     // const currentCategory = currentPhoto.dataset.category;
+    //     // const portfolioStatus = currentPhoto.dataset.portfolio;
+    //     // const allCategoryPhotos = photos.filter(photo => {
+    //     //     if (portfolioStatus === 'true') {
+    //     //         return true;
+    //     //     } else if (portfolioStatus === 'false' && photo.category === currentCategory) {
+    //     //         return true;
+    //     //     }
+    //     //     return false;
+    //     // });
+    //     // const currentPhotoIndex = parseInt(currentPhoto.dataset.id);
+    //     // let nextPhotoIndex;
+    //     // if (currentPhotoIndex + 1 === allCategoryPhotos.length) {
+    //     //     nextPhotoIndex = 0;
+    //     // } else {
+    //     //     nextPhotoIndex = currentPhotoIndex + 1;
+    //     // }
+    //     // const nextPhoto = allCategoryPhotos[nextPhotoIndex];
+    //     // currentPhoto.src = nextPhoto.src;
+    //     // currentPhoto.dataset.id = nextPhotoIndex;
+    //     // const caption = document.querySelector('.photo-caption');
+    //     // caption.textContent = nextPhoto.caption;
+    // }
 
     // all photos:
-    addPhoto('pags-ext.jpg', `Pag's Wine Bar (Doylestown, PA)`, 'commercial');
-    addPhoto('pags-int.jpg', `Pag's Pub (Doylestown, PA)`, 'commercial');
-    addPhoto('pags-int-2.jpg', `Pag's Pub (Doylestown, PA)`, 'commercial');
-    addPhoto('santucci.jpg', `Santucci's Square Pizza (Warminster, PA)`, 'commercial');
-    addPhoto('serenity.jpg', `Serenity Day Spa (Doylestown, PA)`, 'commercial');
+    addPhoto('pags-ext.jpg', `Pag's Wine Bar (Doylestown, PA)`, 'commercial', false);
+    addPhoto('pags-int.jpg', `Pag's Pub (Doylestown, PA)`, 'commercial', true);
+    addPhoto('pags-int-2.jpg', `Pag's Pub (Doylestown, PA)`, 'residential', true);
+    addPhoto('santucci.jpg', `Santucci's Square Pizza (Warminster, PA)`, 'commercial', false);
+    addPhoto('serenity.jpg', `Serenity Day Spa (Doylestown, PA)`, 'commercial', true);
 
     return { 
         getPhotoList,
